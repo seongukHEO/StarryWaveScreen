bdeedb3371abbd41ef3da762f55b4723699f977e
seongukHEO <heoseonguk3@gmail.com>
2024-09-05 04:49:07 +0900

    feat : 집중 화면 구현

--- lib/widget/dowork/focus_widget.dart
+++ lib/widget/dowork/focus_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FocusWidget extends StatelessWidget {
   const FocusWidget({super.key});
@@ -8,8 +9,26 @@ class FocusWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.yellow,
-      ),
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Text('0 개', style: TextStyle(color: Colors.grey),),
+            ),
+            Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset('assets/icon/outlined/goal.svg'),
+                    SizedBox(height: 20,),
+                    Text("아직 추가된 할 일이 없어요!\n아래 버튼으로 새 할 일을 추가해보세요", style: TextStyle(color: Colors.grey),)
+                  ],
+                )
+            )
+          ],
+        ),
+      )
     );
   }
 }
