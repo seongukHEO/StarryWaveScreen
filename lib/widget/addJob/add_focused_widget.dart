54bb63840557af0597e61e72c8b9f96c147060bf
seongukHEO <heoseonguk3@gmail.com>
2024-09-11 04:59:37 +0900

    feat : add_focused_widget.dart 화면 구현

--- lib/widget/addJob/add_focused_widget.dart
+++ lib/widget/addJob/add_focused_widget.dart
@@ -42,6 +42,95 @@ class AddFocusedWidget extends StatelessWidget {
                     ]
                 )
             ),
+            SizedBox(height: 54,),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Container(
+                height: 48,
+                width: 212,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+                  color: Colors.black
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('네! 집중해야 끝낼 수 있어요', style: TextStyle(
+                    fontSize: 15, fontWeight: FontWeight.w700, color: Colors.white
+                  ),),
+                ),
+              ),
+            ),
+            Text("ex) 심리학 과제하기, 코딩 공부하기,,,", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 10, color: Colors.grey),),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Container(
+                height: 48,
+                width: 270,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey)
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('아니요, 조금만 집중해도 할 수 있어요', style: TextStyle(
+                      fontSize: 15, fontWeight: FontWeight.w700, color: Colors.black
+                  ),),
+                ),
+              ),
+            ),
+            Text("ex) 빨래(청소, 설거지)하기, 카톡(메일, 문자)답장하기", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 10, color: Colors.grey),),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: GestureDetector(
+                          onTap: (){},
+                          child: Container(
+                            height: 56,
+                            margin: EdgeInsets.fromLTRB(0, 0, 4, 0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                border: Border.all(),
+                                color: Colors.white
+                            ),
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Text("이전", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 20),)),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: GestureDetector(
+                          onTap: (){},
+                          child: Container(
+                            height: 56,
+                            margin: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                color: Colors.black
+                            ),
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Text("추가하기", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 20),)),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
